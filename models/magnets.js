7ae2b5abafa9be2a8a482d3ae08dc797d70e7bae
tobe_wdw <softwaredeveloper@wdwltd.com>
2025-01-21 17:16:39 +0100

    added the magnets on the db

--- models/magnets.js
+++ models/magnets.js
@@ -1,11 +1,45 @@
 const mongoose = require("mongoose");
 
 const magnetSchema = new mongoose.Schema({
-  type: { type: String, required: true, enum: ["fridge", "car", "custom"] },
   name: { type: String, required: true },
-  title: { type: String, required: true },
-  price: { type: Number, required: true },
+  type: { type: String, required: true, enum: ["fridge", "car", "custom"] },
   image: { type: String, required: true },
   hoverImage: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
+  category: {
+    type: String,
+    required: true,
+    enum: [
+      "valentines-magnet",
+      "birthday-magnet",
+      "mothers-day-magnet",
+      "fathers-day-magnet",
+      "default",
+    ],
+  },
+  slug: { type: String, unique: true, required: false },
 });
+
+magnetSchema.pre("save", async function (next) {
+  console.log("Middle ware is running");
+
+  if (this.isModified("name")) {
+    const baseSlug = this.name
+      .toLowerCase()
+      .replace(/\s+/g, "-")
+      .replace(/[^a-z0-9\-]/g, "");
+    let slug = baseSlug;
+    let count = 0;
+
+    // Ensure slug is unique
+    while (await mongoose.models.Magnet.findOne({ slug })) {
+      count++;
+      slug = `${baseSlug}-${count}`;
+    }
+
+    this.slug = slug;
+  }
+  next();
+});
+
+module.exports = mongoose.model("Magnet", magnetSchema);
--- routes/magnetRoutes.js
+++ routes/magnetRoutes.js
@@ -1,18 +1,33 @@
 const express = require("express");
 const Magnet = require("../models/magnets");
 const router = express.Router();
+const { sizePrices } = require("../data/magnets");
 
 // Add a new magnet
 router.post("/", async (req, res) => {
-  const { type, name, title, price, image, hoverImage } = req.body;
-
-  const magnet = new Magnet({ type, name, title, price, image, hoverImage });
-
   try {
+    const { type, name, image, hoverImage, category, createdAt } = req.body;
+
+    // Basic validation
+    if (!type || !name || !image) {
+      return res.status(400).json({ message: "Missing required fields." });
+    }
+
+    const magnet = new Magnet({
+      name,
+      type,
+      image,
+      hoverImage,
+      category: category || "default",
+      createdAt: createdAt || new Date(),
+    });
+
     const newMagnet = await magnet.save();
+
     res.status(201).json(newMagnet);
   } catch (err) {
-    res.status(400).json({ message: error.message });
+    console.error("Error in POST /api/magnets:", err);
+    res.status(500).json({ message: err.message });
   }
 });
 
@@ -26,14 +41,89 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Get Magnet by type
 router.get("/type/:type", async (req, res) => {
   const { type } = req.params;
 
   try {
-    const magnets = await Magnet.find({ type });
+    const filter = type === "all" ? {} : { type };
+    const magnets = await Magnet.find(filter);
+
     res.json(magnets);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/:slug", async (req, res) => {
+  try {
+    const { slug } = req.params;
+    const magnet = await Magnet.findOne({ slug });
+
+    if (!magnet) {
+      return res.status(404).json({ message: "Magnet not found" });
+    }
+
+    res.json(magnet);
+  } catch (err) {
+    console.error("Error in GET /api/magnets/:slug:", err);
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.delete("/:slug", async (req, res) => {
+  try {
+    const { slug } = req.params;
+
+    const deletedMagnet = await Magnet.findOneAndDelete({ slug });
+
+    if (!deletedMagnet) {
+      return res.status(404).json({ message: "Magnet not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Magnet deleted successfully", magnet: deletedMagnet });
+  } catch (err) {
+    console.error("Error in DELETE /api/magnets/:slug:", err);
+    res
+      .status(500)
+      .json({ message: "An error occurred while deleting the magnet." });
+  }
+});
+
+router.get("/size/sizes", (req, res) => {
+  try {
+    if (!sizePrices) {
+      return res.status(404).json({ message: "No sizes available" });
+    }
+
+    const sizes = Object.keys(sizePrices);
+    res.status(200).json(sizes);
+  } catch (err) {
+    console.error("Error in GET /api/magnets/size/:size:", err);
+    res
+      .status(500)
+      .json({ message: "An error occurred while fetching the sizes." });
+  }
+});
+
+router.get("/size/by-size/:size", (req, res) => {
+  try {
+    const { size } = req.params;
+
+    console.log(sizePrices[size], size, sizePrices["12x12"]);
+
+    if (!sizePrices[size]) {
+      return res.status(404).json({ message: "Size not found." });
+    }
+
+    res.status(200).json({ size, price: sizePrices[size] });
+  } catch (err) {
+    console.error("Error in GET /api/magnets/size/:size:", err);
+    res
+      .status(500)
+      .json({ message: "An error occurred while fetching the price." });
   }
 });
 
